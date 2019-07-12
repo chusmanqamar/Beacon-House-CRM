8e5a3b4cfb7bcb386c136ff68c44c0a8f4fe2553
usmanqamarcis <you@example.comusmanqamar69@gmail.com>
2019-07-12 16:04:28 +0500

    Beacon House CRM

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<div class="pull-right">
+    Product of  <strong>Beaconhouse IT</strong>
+</div>
+<div>
+    <strong>Copyright</strong> BEAMS  &copy; <?php echo date('Y'); ?>-<?php echo date('Y')+1; ?> 
+</div>
